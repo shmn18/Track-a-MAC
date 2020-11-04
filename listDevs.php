7dd1ff4d2fa504f407da588e55eea1598c792a6e
shanmukha raju Mantena <shmn18@student.bth.se>
2020-11-04 10:07:13 +0100

    updated

--- listDevs.php
+++ listDevs.php
@@ -1,15 +1,15 @@
 <?php
-include_once ('config.php');
+include_once('config.php');
 
-$result = $db->query('SELECT * FROM listdevs');
+$res = $db->query('SELECT * FROM info');
 
 
-while ($device_info = $result->fetchArray()) {	
+while ($device_info = $res->fetchArray()) {	
 	echo "\n";
 	echo $device_info[0]. "|" .$device_info[1]. "|" .$device_info[2]. "|" .$device_info[3]. "|" .$device_info[4]. "|" .$device_info[5]. "|" .$device_info[6]."|";
 
     if (empty($device_info[0]) && empty($device_info[1]) && empty($device_info[2]) && empty($device_info[3]) && empty($device_info[4]) && empty($device_info[5]) &&  empty($device_info[6])){
-    	echo "no data";
+    	echo "No MACS";
 
     }
 
@@ -19,3 +19,4 @@ while ($device_info = $result->fetchArray()) {
 $db->close();
 
 ?>
+
