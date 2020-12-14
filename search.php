e26af802c7ea303005955310faf4d27bc339d92b
shanmukha raju Mantena <shmn18@student.bth.se>
2020-12-14 11:51:16 +0100

    searches mac addresses

--- search.php
+++ search.php
@@ -1,39 +1,31 @@
 <?php
-include_once('config.php');
+  include('config.php');
 
 if (empty($_GET)) {
-    echo "It is False";
+    echo "FALSE";
     }
 else {
-    $search = htmlspecialchars($_GET["mac"]);
+    $mac_addr = htmlspecialchars($_GET["mac"]);
     $sql = <<<EOF
-              SELECT * FROM List WHERE MACS LIKE "%$search%" ORDER BY MACS;
+              SELECT * FROM List WHERE MACS LIKE "%$mac_addr%";
 EOF;
-    $output = $db->query($sql);
-    $data = array(); 
-    while($row = $output->fetchArray(SQLITE3_ASSOC) ){
-         #echo $row[1]. "|" . $row[2] . "|" . $row[3] . "|" . $row[4] . "\n";
-         $data[] = $row['IP']. " | " . $row['VLANs'] . " | " . $row['PORT'] . " | " . "$search";
+    $result1 = $db->query($sql);
+    while($row = $result1->fetchArray(SQLITE3_ASSOC) ){
+         $var = echo $row['ip'] . '|' . $row['VLANS'] . '|' . $row['port'] . '|' . $row['MACS'] . "\n";
      
     }
 
-$flag = count($data);
-if($flag ==0){
-    $count = $db->query('SELECT count(*) FROM info');
-    while($check = $count->fetchArray(SQLITE3_ASSOC)) {
-        $no_of_devices = $check['count(*)'];
-        echo "We found no match in $no_of_devices devices"."\n";
+$count = count($var);
+if ($count ==0){
+    $result2 = $db->query('SELECT count(*) FROM Listdevs');
+    while($row1 = $result2->fetchArray(SQLITE3_ASSOC)) {
+        $noDevices = $row1['count(*)'];
+        echo "Not Found in $noDevices devices"."\n";
      }
 }
 
-$result = array_unique($data);
-$total = count($result);
-$i=0;
-while($i<$total){
-    echo $result[$i]. "\n";
-    $i++;
-    }
 }
 $db->close();
 
 ?>
+
