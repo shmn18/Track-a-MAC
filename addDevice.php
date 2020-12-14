80a2d9b3ee813bf6145113e48ab8c5e9b9362ec6
shanmukha raju Mantena <shmn18@student.bth.se>
2020-12-14 11:46:34 +0100

    add works

--- addDevice.php
+++ addDevice.php
@@ -1,23 +1,16 @@
+#!/usr/bin/php
 <?php
-
 include('config.php');
-
 $ip = $_GET['ip'];
 $port = $_GET['port'];
 $community = $_GET['community'];
 $version = $_GET['version'];
-
-if(empty($ip) || empty($port) || empty($community) || empty($version)) {
-    echo "Invalid input" ;   
+if(empty($ip) || empty($port)||empty($community) || empty($version)) {
+echo "FALSE";
 }
-
-else {
-
-    $db->exec("INSERT INTO info (IP,PORT,COMMUNITY,VERSION) VALUES ('$ip','$port','$community','$version')");
-        echo "\n";
-        echo "Ok";
-    
-    }
+else{		
+$db->exec("INSERT INTO LISTDEVS (ip,port,community,version) VALUES ('$ip','$port','$community','$version')");
+echo "OK";	
+}
 $db->close();
 
-?>
