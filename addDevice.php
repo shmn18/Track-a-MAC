86fd6417b2170c84c5589f1c0ca3a3e68fd5d3f8
shanmukha raju Mantena <shmn18@student.bth.se>
2020-12-14 12:17:28 +0100

    adds devices

--- addDevice.php
+++ addDevice.php
@@ -1,16 +1,21 @@
 #!/usr/bin/php
 <?php
-include('config.php');
+
+include_once('config.php');
+
 $ip = $_GET['ip'];
 $port = $_GET['port'];
 $community = $_GET['community'];
 $version = $_GET['version'];
+
 if(empty($ip) || empty($port)||empty($community) || empty($version)) {
-echo "FALSE";
+	echo "FALSE";
 }
+
+
 else{		
-$db->exec("INSERT INTO LISTDEVS (ip,port,community,version) VALUES ('$ip','$port','$community','$version')");
-echo "OK";	
-}
+	$db->exec("INSERT INTO LISTDEVS (ip,port,community,version) VALUES ('$ip','$port','$community','$version')");
+		echo "OK";
+	}
 $db->close();
 
