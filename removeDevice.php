eba669b385dd256d4cfcfabfebec14193cee27a2
shanmukha raju Mantena <shmn18@student.bth.se>
2020-12-14 12:05:22 +0100

    removes devices

--- removeDevice.php
+++ removeDevice.php
@@ -1,15 +1,21 @@
 <?php
-include('config.php');
+include_once('config.php');
 $ip = $_GET['ip'];
 $port = $_GET['port'];
 $community = $_GET['community'];
 $version = $_GET['version'];
 if(empty($ip) || empty($port)||empty($community) || empty($version)) {
-echo "FALSE";
+	echo "FALSE";
 }
-else{		
-$db->exec("DELETE FROM LISTDEVS WHERE ip='$ip' AND port='$port' AND community='$community' AND version='$version'");
-echo "OK";	
+else {		
+	$remove = $db->exec("DELETE FROM LISTDEVS WHERE ip='$ip' AND port='$port' AND community='$community' AND version='$version'");
+	if(!$remove) {
+		echo "false";
+	}
+	else {
+		echo "ok";
+	}	
 }
-$db=null;
+$db->close();
+
 ?>
